be4603bea12b9100b439dd5699b237c29cb2a8d0
bailuoxi66 <bailuoxihaha@163.com>
2022-05-30 13:58:52 +0800

    zinxV0.4-实现全局配置模块

---
+++ demo-zinx/utils/globalobj.go
@@ -0,0 +1,60 @@
+package utils
+
+import (
+	"encoding/json"
+	"github.com/bailuoxi66/go-inside/demo-zinx/ziface"
+	"io/ioutil"
+)
+
+/*
+	存储一切有关Zinx框架的全局参数，供其他模块使用
+	一些参数是可以通过zinx.json由用户进行配置的
+*/
+type GlobalObj struct {
+	/*
+		server
+	*/
+	TcpServer ziface.IServer // 当前Zinx全局的Server对象
+	Host      string         // 当前服务器主机监听的IP
+	TcpPort   int            // 当前服务器主机监听的端口号
+	Name      string         //当前服务器的名称
+	/*
+		zinx
+	*/
+	Version        string // 当前Zinx的版本号
+	MaxConn        int    //当前服务器主机允许的最大链接数
+	MaxPackageSize int    //当前zinx框架数据包的最大值
+}
+
+// 定义一个全局的对外Globalobj
+var GlobalObject *GlobalObj
+
+/*
+	从zinx.json去加载用于自定义的参数
+*/
+func (g *GlobalObj) Reload() {
+	data, err := ioutil.ReadFile("conf/zinx.json")
+	if err != nil {
+		panic(err)
+	}
+
+	// 将json文件解析到struct中
+	err = json.Unmarshal(data, &GlobalObject)
+	if err != nil {
+		panic(err)
+	}
+}
+
+// 提供一个init方法，初始化当前的GlobalObject
+func init() {
+	GlobalObject := &GlobalObj{
+		Name:           "ZinxServerApp",
+		Version:        "V0.4",
+		TcpPort:        8999,
+		Host:           "0.0.0.0",
+		MaxConn:        1000,
+		MaxPackageSize: 4096,
+	}
+
+	GlobalObject.Reload()
+}
