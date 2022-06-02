12073112a61cbdbf145b3ec1697987ad45bdd54a
bailuoxi66 <bailuoxihaha@163.com>
2022-06-02 13:02:32 +0800

    zinxV0.6-使用Zinx框架开发

--- demo-zinx/utils/globalobj.go
+++ demo-zinx/utils/globalobj.go
@@ -47,7 +47,7 @@ func (g *GlobalObj) Reload() {
 func init() {
 	GlobalObject := &GlobalObj{
 		Name:           "ZinxServerApp",
-		Version:        "V0.5",
+		Version:        "V0.6",
 		TcpPort:        8999,
 		Host:           "0.0.0.0",
 		MaxConn:        1000,
