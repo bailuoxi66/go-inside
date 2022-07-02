37548ca2c73843f67e89e32518684e5c0aa20f13
bailuoxi66 <bailuoxihaha@163.com>
2022-07-02 15:26:11 +0800

    AOI兴趣点算法-九宫格单元测试

--- demo_mmo_game_zinx/core/aoi_test.go
+++ demo_mmo_game_zinx/core/aoi_test.go
@@ -12,3 +12,21 @@ func TestNewAOIManager(t *testing.T) {
 	// 打印AOIManager
 	fmt.Println(aoiMgr)
 }
+
+func TestAOIManagerSurroundGridsByGid(t *testing.T) {
+	// 初始化AOIManager
+	aoiMgr := NewAOIManager(0, 250, 5, 0, 250, 5)
+
+	for gid, _ := range aoiMgr.grids {
+		//得到当前gid的九宫格信息
+		grids := aoiMgr.GetSurroundGridsByGid(gid)
+		fmt.Println("gid:", gid, " grids.len:", len(grids))
+		gIDs := make([]int, 0, len(grids))
+		for _, grid := range grids {
+			gIDs = append(gIDs, grid.GID)
+		}
+
+		fmt.Println("surrounding grid IDs are:", gIDs)
+	}
+
+}
